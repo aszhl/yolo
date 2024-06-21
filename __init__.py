f0cc213f2757e65a6e44ce2a02994a7dcdf36d24
aszhl <139355448+aszhl@users.noreply.github.com>
2024-06-21 17:16:47 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,7 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-__version__ = '8.0.207'
+from .rtdetr import RTDETR
+from .sam import SAM
+from .yolo import YOLO
 
-from ultralytics.models import RTDETR, SAM, YOLO
-from ultralytics.models.fastsam import FastSAM
-from ultralytics.models.nas import NAS
-from ultralytics.utils import SETTINGS as settings
-from ultralytics.utils.checks import check_yolo as checks
-from ultralytics.utils.downloads import download
-
-__all__ = '__version__', 'YOLO', 'NAS', 'SAM', 'FastSAM', 'RTDETR', 'checks', 'download', 'settings'
+__all__ = 'YOLO', 'RTDETR', 'SAM'  # allow simpler import
